5af384833e2b111edd45b4fb7365e7ff99fa01bb
takuto.takahashi <takuto.ft222@gmail.com>
2022-09-18 14:59:31 +0900

    first commit.

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-1_agAawJynN', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1jg5e0ce1cdals3pjth1tvtvdf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://00kcenuqfe.execute-api.ap-northeast-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
